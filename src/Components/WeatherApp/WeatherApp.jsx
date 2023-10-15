d9d433265212b1a87075fe16923bc5ced6ea2631
MustWis <147651626+MustWis@users.noreply.github.com>
2023-10-15 10:11:41 +0300

    WeatherApp Integrated

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const WeatherApp = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
+    const api_key = "90ccd00d144edbd275a6c90e474b4660";
+    const city = "Istanbul";
+    const lang = "tr";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric&lang=${lang}`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setWeatherData(response.data);
+        console.log(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Current Weather in Istanbul</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        <div>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherApp;
